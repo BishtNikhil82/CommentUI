73137c1137e12f206562214edcf0f2445e4f56c4
Nikhil Bisht <bist.nikhil@gmail.com>
2025-07-30 13:23:35 +0530

    remove force ssl scope

--- src/lib/google-auth.ts
+++ src/lib/google-auth.ts
@@ -44,7 +44,6 @@ export const GOOGLE_OAUTH_CONFIG = {
     'https://www.googleapis.com/auth/userinfo.email',
     'https://www.googleapis.com/auth/userinfo.profile',
     'https://www.googleapis.com/auth/youtube.readonly',
-    'https://www.googleapis.com/auth/youtube.force-ssl'
   ].join(' ')
 };
 
